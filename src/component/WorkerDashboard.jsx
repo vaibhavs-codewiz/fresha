fc5ace81c84838dbcf5d69a004329e5b48adae34
Nipun899 <nipunjain899@gmail.com>
2024-09-27 12:55:58 +0530

    Your commit message

--- src/component/WorkerDashboard.jsx
+++ src/component/WorkerDashboard.jsx
@@ -1,97 +1,115 @@
-import "../styles/WorkerDashboard.css"
-import "../styles/Details.css"
-const appointments = [
-    { serviceName: "Hair Cut, Spa, Nails", clientName: "Amanda", contact: "9026671648", dateTime: "12.09.2024 - 12.53 PM", duration: "1h 15min" },
-    { serviceName: "Spa, Makeup", clientName: "Arjun", contact: "9026671648", dateTime: "03.05.2024 - 10.53 PM", duration: "45min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" },
-    { serviceName: "Nails", clientName: "Kresha", contact: "9026671648", dateTime: "25.05.2025 - 02.53 PM", duration: "15min" }
-];
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./WorkerDashboard.css";
+import "./Details.css";
 
 function WorkerDashboard() {
-    return (
-        <div className="worker-container">
-            <aside className="sidebar">
-                <div >
-                    <div className="logo">
-                        <div className="circle"></div>
-                    </div>
-                    <div className="logo2">
-                        <div className="circle"></div>
-                    </div>
-                </div>
-                <div className="sidebar2">
-                    <nav className="navigation">
-                        <ul>
-                            <li className="active">Appointments</li>
-                            <li>Stock Management</li>
-                        </ul>
-                    </nav>
-                    <div className="bottom">
-                        <button>Settings</button>
-                        <button>Logout</button>
-                    </div>
-                </div>
-            </aside>
+  const [appointments, setAppointments] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/appointment/getAllAppointments",
+          {
+            withCredentials: true, // For cookies, if needed
+          }
+        );
 
-            <div className="main">
-                <div className="worker-header">
-                    <div className="search-bar">
-                        <input type="text" placeholder="Search" />
-                    </div>
-                    <div className="profile">
-                        <span className="notification">
-                            <i className="fa fa-bell"></i>
-                            <span className="badge">2</span>
-                        </span>
-                        <div className="user-info">
-                            <img src="https://via.placeholder.com/50" alt="User" />
-                            <div>
-                                <p>Pavitra Gupta<br /></p>
-                                <p className="role">Admin</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+        setAppointments(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-                <div className="worker-mainDetail">
-                    <div className="worker-inner-container">
-                        <div className="worker-item1">
-                    <h2>Your Appointment</h2>
-                    <div className="inner-header">
-                        <div className="services">Image</div>
-                        <div className="services">Service </div>
-                        <div className="services">Price</div>
-                        <div className="services">Duration</div>
-                        <div className="services">Action </div>
+    fetchData();
+  }, []);
+
+  let navigate = useNavigate();
+  function handleNavigation() {
+    navigate("/Booking");
+  }
+  return (
+    <div className="worker-container">
+      <aside className="sidebar">
+        <div>
+          <div className="logo">
+            <div className="circle"></div>
+          </div>
+          <div className="logo2">
+            <div className="circle"></div>
+          </div>
+        </div>
+        <div className="sidebar2">
+          <nav className="navigation">
+            <ul>
+              <li className="active">Appointments</li>
+              <li>Stock Management</li>
+            </ul>
+          </nav>
+          <div className="bottom">
+            <button>Settings</button>
+            <button>Logout</button>
+          </div>
+        </div>
+      </aside>
+
+      <div className="main">
+        <div className="worker-header">
+          <div className="search-bar">
+            <input type="text" placeholder="Search" />
+          </div>
+          <div className="profile">
+            <span className="notification">
+              <i className="fa fa-bell"></i>
+              <span className="badge">2</span>
+            </span>
+            <div className="user-info">
+              <img src="https://via.placeholder.com/50" alt="User" />
+              <div>
+                <p>
+                  Pavitra Gupta
+                  <br />
+                </p>
+                <p className="role">Admin</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="worker-mainDetail">
+          <div className="worker-inner-container">
+            <div className="worker-item1">
+              <h2>Your Appointment</h2>
+              <div className="inner-header">
+                <div className="services">Image</div>
+                <div className="services">Service </div>
+                <div className="services">Price</div>
+                <div className="services">Duration</div>
+                <div className="services">Action </div>
+              </div>
+              <div className="parent">
+                {appointments.map((appointment, index) => (
+                  <div className="detailsCard" key={index}>
+                    <div className="map">
+                      <div className="mapChilds">
+                        {" "}
+                        {appointment.service_id.name}{" "}
+                      </div>
+                      <div className="mapChilds">{appointment.user_id.name} </div>
+                      <div className="mapChilds"> {appointment.price} </div>
+                      <div className="mapChilds">
+                        {appointment.appointment_time}
+                      </div>
+                      <div className="mapChilds">
+                        {" "}
+                        <button> {appointment.status} </button>{" "}
+                      </div>
                     </div>
-                        <div className="parent">
-                            {appointments.map((service, index) => (
-                                <div className="detailsCard" key={index}>
-                                    <div className="map">
-                                        <div className="mapChilds"> {service.serviceName} </div>
-                                        <div className="mapChilds">{service.clientName} </div>
-                                        <div className="mapChilds"> {service.contact} </div>
-                                        <div className="mapChilds">{service.dateTime}</div>
-                                        <div className="mapChilds">
-                                            {" "}
-                                            <button > Book </button>{" "}
-                                        </div>
-                                    </div>
-                                </div>
-                            ))}
-                        </div>
-                        {/* <tbody>
+                  </div>
+                ))}
+              </div>
+              {/* <tbody>
                             {appointments.map((appointment, index) => (
                                 <>
                                     <tr key={index}>
@@ -106,13 +124,12 @@ function WorkerDashboard() {
                                 </>
                             ))}
                         </tbody> */}
-                    
-                        </div>
-                    </div>
-                </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default WorkerDashboard;
